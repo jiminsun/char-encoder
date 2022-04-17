eafc751d63834c53bcb2c9c931b136805495932c
jiminsun <jiminxsun@gmail.com>
2022-04-17 00:35:19 -0400

    Add tydiqa language codes

---
+++ xtreme/third_party/adapters/task_languages.py
@@ -0,0 +1,27 @@
+TYDIQA_GOLDP_LANGS = {
+    'ar': 0,
+    'bn': 1,
+    'en': 2,
+    'fi': 3,
+    'id': 4,
+    'ko': 5,
+    'ru': 6,
+    'sw': 7,
+    'te': 8
+}
+
+TYDIQA_GOLDP_ID2LANG = ['ar', 'bn', 'en', 'fi', 'id', 'ko', 'ru', 'sw', 'te']
+
+TYDIQA_PRIMARY_LANGS = {
+    'ar': 0,
+    'bn': 1,
+    'en': 2,
+    'fi': 3,
+    'id': 4,
+    'ko': 5,
+    'ru': 6,
+    'sw': 7,
+    'te': 8,
+    'ja': 9,
+    'th': 10
+}

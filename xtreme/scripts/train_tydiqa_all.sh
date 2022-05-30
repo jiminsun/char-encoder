3a25700c1b2dbed3214add66bbc9d8e9054e7050
jiminsun <jiminxsun@gmail.com>
2022-05-29 21:53:41 -0400

    multilingual tydiqa training

--- xtreme/scripts/train_tydiqa_all.sh
+++ xtreme/scripts/train_tydiqa_all.sh
@@ -13,37 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Script to train a model on SQuAD v1.1 or the English TyDiQA-GoldP train data.
+# Script to train a model on the TyDiQA-GoldP train data of all languages.
 
 REPO=$PWD
 MODEL=${1:-bert-base-multilingual-cased}
-SRC=${2:-squad}
-TGT=${3:-xquad}
-GPU=${4:-0}
-DATA_DIR=${5:-"$REPO/download/"}
-OUT_DIR=${6:-"$REPO/outputs/"}
+GPU=${2:-0}
+DATA_DIR=${3:-"$REPO/download/"}
+OUT_DIR=${4:-"$REPO/outputs/"}
 
+SRC=tydiqa
 BATCH_SIZE=8
-GRAD_ACC=64
+GRAD_ACC=4
 
 #MAXL=384
-LR=5e-5
-NUM_EPOCHS=10
+LR=3e-5
+MAX_STEPS=50000
 
 if [ $MODEL == "bert-base-multilingual-cased" ]; then
   MODEL_TYPE="bert"
-  MAXL=512
-  MAX_QUERY_LEN=64
-  MAX_ANSWER_LEN=30
-  DOC_STRIDE=128
-elif [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-mlm-tlm-xnli15-1024" ]; then
-  MODEL_TYPE="xlm"
-  MAXL=512
-  MAX_QUERY_LEN=64
-  MAX_ANSWER_LEN=30
-  DOC_STRIDE=128
-elif [ $MODEL == "xlm-roberta-large" ] || [ $MODEL == "xlm-roberta-base" ]; then
-  MODEL_TYPE="xlm-roberta"
   MAXL=512
   MAX_QUERY_LEN=64
   MAX_ANSWER_LEN=30
@@ -54,32 +41,21 @@ elif [ $MODEL == "google/canine-s" ] || [ $MODEL == "google/canine-c" ]; then
   MAX_QUERY_LEN=256
   MAX_ANSWER_LEN=100
   DOC_STRIDE=512
-elif [ $MODEL == "google/mt5-small" ] || [ $MODEL == "google/mt5-base" ] || [ $MODEL == "google/mt5-large" ]; then
-  MODEL_TYPE="mt5"
-  MAXL=1024
-  MAX_QUERY_LEN=128
-  MAX_ANSWER_LEN=30
-  DOC_STRIDE=256
-elif [ $MODEL == "google/byt5-small" ] || [ $MODEL == "google/byt5-base" ] || [ $MODEL == "google/byt5-large" ]; then
-  MODEL_TYPE="byt5"
-  MAXL=1024
-  MAX_QUERY_LEN=128
-  MAX_ANSWER_LEN=100
-  DOC_STRIDE=256
 fi
 
 # Model path where trained model should be stored
-MODEL_PATH=$OUT_DIR/$SRC/all_langs_${MODEL}_LR${LR}_EPOCH${NUM_EPOCHS}_maxlen${MAXL}_batchsize${BATCH_SIZE}_gradacc${GRAD_ACC}
+MODEL_PATH=$OUT_DIR/$SRC/multilingual/${MODEL}_LR${LR}_EPOCH${NUM_EPOCHS}_maxlen${MAXL}_batchsize${BATCH_SIZE}_gradacc${GRAD_ACC}
 mkdir -p $MODEL_PATH
 
 TASK_DATA_DIR=${DATA_DIR}/tydiqa
-TRAIN_FILE=${TASK_DATA_DIR}/tydiqa-goldp-v1.1-train/tydiqa.goldp.train.json
-PREDICT_FILE=${TASK_DATA_DIR}/tydiqa-goldp-v1.1-dev/tydiqa.goldp.dev.json
+TRAIN_FILE=${TASK_DATA_DIR}/tydiqa-goldp-v1.1-train/tydiqa-goldp-v1.1-train.json
+PREDICT_FILE=${TASK_DATA_DIR}/tydiqa-goldp-v1.1-dev/tydiqa.en.dev.json
 
 # train
 CUDA_VISIBLE_DEVICES=$GPU python third_party/run_squad.py \
   --model_type ${MODEL_TYPE} \
   --model_name_or_path ${MODEL} \
+  --task_name ${SRC} \
   --do_train \
   --do_eval \
   --data_dir ${TASK_DATA_DIR} \
@@ -87,20 +63,22 @@ CUDA_VISIBLE_DEVICES=$GPU python third_party/run_squad.py \
   --predict_file ${PREDICT_FILE} \
   --per_gpu_train_batch_size ${BATCH_SIZE} \
   --learning_rate ${LR} \
-  --num_train_epochs ${NUM_EPOCHS} \
+  --max_steps ${MAX_STEPS} \
   --max_seq_length ${MAXL} \
   --max_query_length ${MAX_QUERY_LEN} \
   --max_answer_length ${MAX_ANSWER_LEN} \
   --doc_stride ${DOC_STRIDE} \
-  --save_steps -1 \
-  --overwrite_output_dir \
+  --save_steps 10000 \
+  --overwrite_output_dir --overwrite_cache \
   --gradient_accumulation_steps ${GRAD_ACC} \
-  --warmup_steps 500 \
+  --warmup_steps 0.1 \
   --output_dir ${MODEL_PATH} \
   --weight_decay 0.0001 \
   --threads 8 \
-  --train_lang en \
-  --eval_lang en
+  --local_rank -1 \
+  --train_lang all \
+  --eval_lang en --fp16
 
 # predict
-bash scripts/predict_qa.sh $MODEL $MODEL_PATH $TGT $GPU $DATA_DIR
+bash scripts/evaluate_tydiqa_all.sh $MODEL tydiqa tydiqa $GPU $DATA_DIR
+

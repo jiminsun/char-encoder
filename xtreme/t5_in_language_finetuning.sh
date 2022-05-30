5a93b2b40e4005301d4ac49880164a82588d1f57
jiminsun <jiminxsun@gmail.com>
2022-05-29 21:10:53 -0400

    add tgt lang finetuning script

--- xtreme/t5_in_language_finetuning.sh
+++ xtreme/t5_in_language_finetuning.sh
@@ -5,7 +5,7 @@ export WANDB_API_KEY="2f0079981993b2ca38c42cb4ad8df1a168394072"
 
 REPO=$PWD
 
-MODEL=${1:-bert-base-multilingual-cased}
+MODEL=${1:-google/mt5-small}
 TASK=${2:-tydiqa}
 GPU=${3:-0}
 DATA_DIR=${4:-"$REPO/download/"}
@@ -15,17 +15,13 @@ OUT_DIR=${5:-"$REPO/outputs/"}
 if [ $TASK == 'tydiqa' ]; then
   LANGS=( "en" "ko" "sw" "ar" "bn" "fi" "id" "ru" "te" )
 elif [ $TASK == 'xnli' ]; then
-  LANGS=( "ar" "en" "bg" "de" "el" "es" "fr" "hi" "ru" "sw" "th" "tr" "ur" "vi" "zh" )
-elif [ $TASK == 'panx' ]; then
+  LANGS=( "en" "ar" "bg" "de" "el" "es" "fr" "hi" "ru" "sw" "th" "tr" "ur" "vi" "zh" )
+elif [ $TASK == 'ner' ]; then
   LANGS=( "en" "hi" "bn" "ur" "sw" "ar" "de" "el" "es" "fi" "fr" "ko" "ru" "te" "zh" "tr" "ta" "id" )
 fi
-
-
-MODELS=( "bert-base-multilingual-cased" "google/canine-s" "google/canine-c" )
-
 
 for LANG in "${LANGS[@]}"; do
   echo "Fine-tuning $MODEL on $TASK using GPU $GPU"
   echo "Load data from $DATA_DIR, and save models to $OUT_DIR"
-  bash $REPO/scripts/finetune_${TASK}.sh $MODEL $LANG $GPU $DATA_DIR $OUT_DIR
+  bash $REPO/t5_scripts/train_${TASK}.sh $MODEL $GPU $LANG $LANG $DATA_DIR $OUT_DIR
 done

896339ee4594cfa128230fccf3f9ef5cc62aa446
jiminsun <jiminxsun@gmail.com>
2022-05-29 21:03:33 -0400

    add tydiqa training

--- xtreme/scripts/finetune_tydiqa.sh
+++ xtreme/scripts/finetune_tydiqa.sh
@@ -21,54 +21,35 @@ LANG=${2:-en}
 GPU=${3:-0}
 DATA_DIR=${4:-"$REPO/download/"}
 OUT_DIR=${5:-"$REPO/outputs/"}
+# FREEZE=${6:-"--freeze_embedding"}
 
 TASK=tydiqa
 
-BATCH_SIZE=4
-GRAD_ACC=8
+BATCH_SIZE=8
+GRAD_ACC=4
 
 #MAXL=384
 LR=3e-5
-NUM_EPOCHS=5.0
+NUM_EPOCHS=20
 if [ $MODEL == "bert-base-multilingual-cased" ]; then
   MODEL_TYPE="bert"
   MAXL=512
   MAX_QUERY_LEN=64
   MAX_ANSWER_LEN=30
   DOC_STRIDE=128
-elif [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-mlm-tlm-xnli15-1024" ]; then
-  MODEL_TYPE="xlm"
-  MAXL=512
-  MAX_QUERY_LEN=64
-  MAX_ANSWER_LEN=30
-  DOC_STRIDE=128
-elif [ $MODEL == "xlm-roberta-large" ] || [ $MODEL == "xlm-roberta-base" ]; then
-  MODEL_TYPE="xlm-roberta"
-  MAXL=512
-  MAX_QUERY_LEN=64
-  MAX_ANSWER_LEN=30
-  DOC_STRIDE=128
+  MAX_STEPS=5000
 elif [ $MODEL == "google/canine-s" ] || [ $MODEL == "google/canine-c" ]; then
   MODEL_TYPE="canine"
   MAXL=2048
   MAX_QUERY_LEN=256
   MAX_ANSWER_LEN=100
   DOC_STRIDE=512
-elif [ $MODEL == "google/mt5-small" ] || [ $MODEL == "google/mt5-base" ] || [ $MODEL == "google/mt5-large" ]; then
-  MODEL_TYPE="mt5"
-  MAXL=1024
-  MAX_QUERY_LEN=128
-  DOC_STRIDE=256
-elif [ $MODEL == "google/byt5-small" ] || [ $MODEL == "google/byt5-base" ] || [ $MODEL == "google/byt5-large" ]; then
-  MODEL_TYPE="byt5"
-  MAXL=1024
-  MAX_QUERY_LEN=128
-  DOC_STRIDE=256
+  MAX_STEPS=5000
 fi
 
 
 # Model path where trained model should be stored
-MODEL_PATH=$OUT_DIR/${TASK}/${LANG}_${MODEL}_LR${LR}_EPOCH${NUM_EPOCHS}_maxlen${MAXL}_batchsize${BATCH_SIZE}_gradacc${GRAD_ACC}
+MODEL_PATH=$OUT_DIR/${TASK}/in_language/${LANG}_${MODEL}_LR${LR}_maxsteps${MAX_STEPS}_maxlen${MAXL}_batchsize${BATCH_SIZE}_gradacc${GRAD_ACC}
 mkdir -p $MODEL_PATH
 
 # Train on TyDiQa-GoldP $LANG train file
@@ -83,25 +64,27 @@ CUDA_VISIBLE_DEVICES=$GPU python third_party/run_squad.py \
   --model_name_or_path ${MODEL} \
   --do_train \
   --do_eval \
+  --overwrite_cache \
   --data_dir ${TASK_DATA_DIR} \
   --train_file ${TRAIN_FILE} \
   --predict_file ${PREDICT_FILE} \
   --per_gpu_train_batch_size ${BATCH_SIZE} \
   --learning_rate ${LR} \
   --num_train_epochs ${NUM_EPOCHS} \
+  --max_steps ${MAX_STEPS} \
   --max_seq_length ${MAXL} \
   --max_query_length ${MAX_QUERY_LEN} \
   --max_answer_length ${MAX_ANSWER_LEN} \
   --doc_stride ${DOC_STRIDE} \
-  --save_steps -1 \
+  --save_steps 200 \
   --overwrite_output_dir \
   --gradient_accumulation_steps ${GRAD_ACC} \
-  --warmup_steps 500 \
+  --warmup_steps 0.1 \
   --output_dir ${MODEL_PATH} \
   --weight_decay 0.0001 \
   --threads 8 \
   --train_lang ${LANG} \
-  --eval_lang ${LANG}
+  --eval_lang ${LANG} --fp16
 
 # predict
-bash scripts/predict_qa.sh $MODEL $MODEL_PATH $TASK $GPU $DATA_DIR
+# bash scripts/predict_qa.sh $MODEL $MODEL_PATH $TASK $GPU $DATA_DIR

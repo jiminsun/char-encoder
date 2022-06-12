96fbd4acbe8e9061671324e85e1e106b4ccb83e2
jiminsun <jiminxsun@gmail.com>
2022-06-11 23:37:22 -0400

    add xnli t5 testing script

--- xtreme/t5_scripts/test_xnli.sh
+++ xtreme/t5_scripts/test_xnli.sh
@@ -5,13 +5,19 @@ GPU=${3:-0}
 
 DATA_DIR=$REPO/download/
 TASK=xnli
-BATCH_SIZE=8
 
-MAXL=4096
-MAX_ANSWER_LEN=8
+if grep -q "byt5" <<< "$MODEL"; then
+    MAXL=4096
+    MAX_ANSWER_LEN=8
+    BATCH_SIZE=16
+else
+    MAXL=2048
+    MAX_ANSWER_LEN=4
+    BATCH_SIZE=16
+fi
 
 if [ $LANG == "all" ]; then
-  LANGS=( "ar" "en" "bg" "de" "el" "es" "fr" "hi" "ru" "sw" "th" "tr" "ur" "vi" "zh" )
+  LANGS=( "en" "ar" "bg" "de" "el" "es" "fr" "hi" "ru" "sw" "th" "tr" "ur" "vi" "zh" )
   for L in "${LANGS[@]}"; do
     echo "=== evaluating on $L"
     CUDA_VISIBLE_DEVICES=$GPU python third_party/run_t5_classification.py \
@@ -30,7 +36,7 @@ if [ $LANG == "all" ]; then
       --generation_num_beams 5
   done
 else
-  echo "=== evaluating on $L"
+  echo "=== evaluating on $LANG"
     CUDA_VISIBLE_DEVICES=$GPU python third_party/run_t5_classification.py \
       --model_name_or_path ${MODEL} \
       --task ${TASK} \
